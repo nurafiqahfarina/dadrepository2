8c5055f899c0394d7ce6b268d303d643d9c3b778
nurafiqahfarina <92964719+nurafiqahfarina@users.noreply.github.com>
2022-02-03 17:50:31 +0800

    Recently created
    This is a new repository

---
+++ workspace-dadlabs/orderapp/src/main/java/demo/ftmk/utem/DateServlet.java
@@ -0,0 +1,71 @@
+package demo.ftmk.utem;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DateServlet
+ */
+@WebServlet("/dateServlet")
+public class DateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Get link parameter
+		
+		int paramValue=Integer.parseInt(request.getParameter("param"));
+		
+		//Get writer
+		PrintWriter writer = response.getWriter();
+		
+		//Get current date
+		LocalDate now = LocalDate.now();
+		
+		if(paramValue == 0) {
+			
+			//Display current date
+			writer.println("Today is "+now.toString());
+			
+		}
+		else if(paramValue == 1) {
+			
+			//Display tommorow date
+			LocalDate tomorrow = now.plusDays(1);
+			writer.println("Tomorrow is "+tomorrow.toString());
+		}
+		else if(paramValue == -1) {
+			
+			//Display yesterday date
+			LocalDate yesterday = now.minusDays(1);
+			writer.println("Yesterday is "+yesterday.toString());
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
